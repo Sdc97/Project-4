630abf20c9042c9a6b2089748721edae5d0406fb
neoha <neoha@DESKTOP-3BMB1G7>
2018-05-29 11:38:13 -0700

    main

---
+++ src/sync/MainMenu.java
@@ -0,0 +1,26 @@
+package sync;
+
+import java.util.*;
+public class MainMenu 
+{
+
+	public static void main(String[] args) 
+	{
+		String choice;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Initialize Games. Please wait...");
+		System.out.println("All games are ready.");
+		//System.out.println("Available games: ");
+
+		System.out.println("Main Menu\n1. Select a game\n"
+				+ "2. Add a new player to the list\n3. Quit");
+		
+		//while(choice)
+	}
+
+	private static Scanner Scanner(InputStream in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
